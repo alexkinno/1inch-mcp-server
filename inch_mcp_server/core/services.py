eff7912dd30ccb86bc8759bd15be2afc9987cbdf
alla-l-innowise <alla.levental@innowise.com>
2025-08-01 21:41:04 +0200

    fix

--- inch_mcp_server/core/services.py
+++ inch_mcp_server/core/services.py
@@ -18,15 +18,16 @@ async def fetch_and_store_orders(chain: int, address: str):
     logger.info("Fetched {} orders".format(len(orders)))
     retrieved_hashes = {order.orderHash for order in orders}
     logger.info("retrieved from api: {}".format(retrieved_hashes))
-    query = select(LimitOrder).where((LimitOrder.blockchain_id == chain) & (LimitOrder.address == address))
+    # query = select(LimitOrder).where((LimitOrder.blockchain_id == chain) & (LimitOrder.address == address))
+    query = select(LimitOrder)
     result = (await db.session.scalars(query)).all()
     stored_hashes = {order.order_hash for order in result}
     logger.info("retrieved from db: {}".format(stored_hashes))
     hashes_to_delete = list(stored_hashes - retrieved_hashes)
     hashes_to_return = list(retrieved_hashes & stored_hashes)
     logger.info("created: {}".format(hashes_to_return))
+    logger.info("outdated: {}".format(hashes_to_delete))
     if hashes_to_delete:
-        logger.info("outdated: {}".format(hashes_to_delete))
         await db.session.execute(delete(LimitOrder).where(LimitOrder.order_hash.in_(hashes_to_delete)))
         await db.session.commit()
     orders = [order for order in orders if order.orderHash in hashes_to_return]
@@ -58,4 +59,5 @@ async def post_order(chain: int, order_data: PostLimitOrderV4Request):
 async def fetch_all_orders():
     query = select(LimitOrder)
     result = (await db.session.scalars(query)).all()
+    logger.info("all {}".format(result))
     return result
